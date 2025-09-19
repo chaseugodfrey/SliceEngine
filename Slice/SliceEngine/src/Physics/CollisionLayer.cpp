f51aa3af984884aa4a00bad4329ef778e46d3055
AloysiusTeo <teo.k@digipen.edu>
2025-09-19 15:52:09 +0800

    temp layer impl to get shit working

--- Slice/SliceEngine/src/Physics/CollisionLayer.cpp
+++ Slice/SliceEngine/src/Physics/CollisionLayer.cpp
@@ -1,5 +1,64 @@
 #include <pch.h>
 #include "CollisionLayer.h"
 
+namespace SliceEngine 
 
+{
+
+	BPLayerInterfaceImpl::BPLayerInterfaceImpl()
+	{
+		mObjectToBroadPhase[Layers::NON_MOVING] = BPLayers::NON_MOVING;
+		mObjectToBroadPhase[Layers::MOVING] = BPLayers::MOVING;
+	}
+
+	JPH::uint BPLayerInterfaceImpl::GetNumBroadPhaseLayers()
+	{
+		return BPLayers::NUM_LAYERS;
+	}
+
+	JPH::BroadPhaseLayer BPLayerInterfaceImpl::GetBroadPhaseLayer(JPH::ObjectLayer inLayer)
+	{
+		return mObjectToBroadPhase[inLayer];
+	}
+
+	#if defined(JPH_EXTERNAL_PROFILE) || defined(JPH_PROFILE_ENABLED)
+
+	const char* BPLayerInterfaceImpl::GetBroadPhaseLayerName(JPH::BroadPhaseLayer inLayer) const
+	{
+		return "lol"; // leave empty for now
+	}
+
+	#endif
+	// Class that determines if object layer collides with a broadphaselayer
+	bool ObjectVsBroadPhaseLayerFilterImpl::ShouldCollide(JPH::ObjectLayer inObject1, JPH::BroadPhaseLayer inLayer2) const
+	{
+		switch (inObject1)
+		{
+		case Layers::NON_MOVING:
+			return inLayer2 == BPLayers::MOVING;
+		case Layers::MOVING:
+			return true;
+		default:
+			JPH_ASSERT(false);
+			return false;
+		}
+	}
+
+	// Class that determines if object layer can collide
+	bool ObjectLayerPairFilterImpl::ShouldCollide(JPH::ObjectLayer inObject1, JPH::ObjectLayer inObject2) const
+	{
+		switch (inObject1)
+		{
+		case Layers::NON_MOVING:
+			return inObject2 == Layers::MOVING;
+		case Layers::MOVING:
+			return true;
+		default:
+			JPH_ASSERT(false);
+			return false;
+		}
+	}
+
+
+}
 
