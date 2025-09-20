8542ac97df93b2f8ba1244eedee2b6164c1e85b6
Gideon Francis <gideonnfrancis999@gmail.com>
2025-09-21 06:38:53 +0800

    Figuring out resource handling :pray: :pray:

--- Slice/SliceEngine/src/Resource/ResourceManager.h
+++ Slice/SliceEngine/src/Resource/ResourceManager.h
@@ -11,6 +11,36 @@ namespace SliceEngine
 {
 
 	class ResourceManager;
+
+	template <typename T> class Handle;
+
+	template <typename ResourceType>
+	struct Type
+	{
+		// Each specialization MUST provide:
+		// constexpr static inline uint64_t typeUUID = YOUR_UNIQUE_ID;
+		// constexpr static inline uint64_t defaultResourceGUID = YOUR_DEFAULT_ID;
+		// static ResourceType* Load(ResourceManager&, uint64_t guid, const std::string& path);
+		// static void Destroy(ResourceType&, ResourceManager&);
+	};
+
+	namespace detail
+	{
+		struct Instance
+		{
+			void* data = nullptr;
+			int refCount = 1;
+			void (*destroyer)(void*, ResourceManager&) = nullptr;
+		};
+	}
+
+	class ResourceManager
+	{
+	public:
+		ResourceManager() = default;
+		~ResourceManager();
+	private:
+	};
 }
 
 
