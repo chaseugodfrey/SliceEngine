69cf3988ea4982b4e04e4ada8fb9e05de49aec8f
chaseugodfrey <143150195+chaseugodfrey@users.noreply.github.com>
2025-09-20 21:20:08 +0800

    GUID stuff :pray: :cry: :cry: :scream: - gideon

---
+++ Slice/SliceEngine/src/Resource/GUID.h
@@ -0,0 +1,54 @@
+#ifndef GUID_H
+#define GUID_H
+
+#include <cstdint>
+#include <functional>
+#include <random>
+#include <rttr/registration.h>
+
+namespace SliceEngine
+{
+	struct GUID
+	{
+	public:
+		GUID() : mValue(0) {}
+		explicit GUID(uint64_t value) : mValue(value) {}
+		bool operator==(const GUID& other) const { return mValue == other.mValue; }
+		bool operator!=(const GUID& other) const { return mValue != other.mValue; }
+		bool operator<(const GUID& other) const { return mValue < other.mValue; }
+		bool operator<=(const GUID& other) const { return mValue <= other.mValue; }
+		bool operator>(const GUID& other) const { return mValue > other.mValue; }
+		bool operator>=(const GUID& other) const { return mValue >= other.mValue; }
+
+		static GUID Generate()
+		{
+			static std::mt19937_64 rng(std::random_device{}());
+			static std::uniform_int_distribution<uint64_t> dist;
+			return GUID(dist(rng) | (1ULL << 63)); // Ensure first bit is 1
+		}
+
+		struct Hasher
+		{
+			std::size_t operator()(const GUID& guid) const noexcept
+			{
+				return std::hash<uint64_t>{}(guid.mValue);
+			}
+		};
+
+		uint64_t GetGUID() const { return mValue; }
+
+	private:
+		uint64_t mValue;
+	};
+
+	RTTR_REGISTRATION
+	{
+		rttr::registration::class_<GUID>("GUID")
+			.constructor<>()
+			.constructor<uint64_t>()
+			.property("Value", &GUID::GetGUID);
+	}
+}
+
+
+#endif // !GUID_H
---
+++ Slice/SliceEngine/src/Resource/ResourceManager.h
@@ -0,0 +1,19 @@
+#ifndef RESOURCE_MANAGER_H
+#define RESOURCE_MANAGER_H
+#include <string>
+#include <unordered_map>
+#include <memory>
+#include <functional>
+#include <rttr/type.h>
+#include "GUID.h"
+
+namespace SliceEngine
+{
+
+	class ResourceManager;
+}
+
+
+
+
+#endif
