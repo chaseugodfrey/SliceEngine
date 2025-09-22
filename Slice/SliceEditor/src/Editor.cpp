b63e1e766d14c32cb21f05b79f59461aebc86448
nic <nicklaiabc123@gmail.com>
2025-09-22 10:49:43 +0800

    Re-enabled ImGui config

--- Slice/SliceEditor/src/Editor.cpp
+++ Slice/SliceEditor/src/Editor.cpp
@@ -62,7 +62,7 @@ namespace SliceEditor
 		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
 		io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
 		io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
-		io.IniFilename = nullptr;
+		//io.IniFilename = nullptr; //To disable the config for ImGui
 
 		// Setup Platform/Renderer backends
 		ImGui_ImplGlfw_InitForOpenGL(window, true);          // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
