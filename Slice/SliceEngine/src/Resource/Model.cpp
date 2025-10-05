830b460f08656a1f7a8791554fbf05e65b335b6c
Elton <leosantosa.elton@gmail.com>
2025-10-05 20:29:45 +0800

    oops forgot this

--- Slice/SliceEngine/src/Resource/Model.cpp
+++ Slice/SliceEngine/src/Resource/Model.cpp
@@ -114,8 +114,6 @@ namespace SliceEngine
 			glEnableVertexArrayAttrib(vao, 0);
 			glEnableVertexArrayAttrib(vao, 1);
 			glEnableVertexArrayAttrib(vao, 2);
-			glEnableVertexArrayAttrib(vao, 3);
-			glEnableVertexArrayAttrib(vao, 4);
 			glVertexArrayAttribFormat(vao, 0, 3, GL_FLOAT, false, offsetof(Vertex, position));
 			glVertexArrayAttribFormat(vao, 1, 3, GL_FLOAT, false, offsetof(Vertex, normal));
 			glVertexArrayAttribFormat(vao, 2, 2, GL_FLOAT, false, offsetof(Vertex, uv));
