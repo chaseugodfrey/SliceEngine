3958cf32662e7002d33be9c2d634db13d1c8593f
Rain <47514829+NivleMW@users.noreply.github.com>
2025-10-05 23:25:07 +0800

    my stuff header comments

--- Slice/SliceEngine/src/Graphics/RenderManager.h
+++ Slice/SliceEngine/src/Graphics/RenderManager.h
@@ -1,6 +1,15 @@
-/*
-*	Functions here assumes CreateCamera is called once, and therefore mainCam has value
-*/
+/*-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ file:			RenderManager.cpp
+ author:		Won Yu Xuan Rainne
+ email:			won.m@digipen.edu
+ brief:			Handles the Rendering pipeline, and related things
+
+				Functions here assumes CreateCamera is called once, and therefore mainCam has value
+
+Copyright (C) 2024 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents without the prior written consent of
+DigiPen Institute of Technology is prohibited.
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
 #ifndef RENDER_MANAGER_H
 #define RENDER_MANAGER_H
