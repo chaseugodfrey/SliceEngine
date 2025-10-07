194340f2647a11e2b9003657e81e564a193e86e3
Rain <47514829+NivleMW@users.noreply.github.com>
2025-10-07 20:32:33 +0800

    Change to BRDF_MultipleLights
    Basically assumes has 1 directional light ngl
    
    Also, cap at 32 lights

--- Slice/SliceEngine/src/Graphics/LightingSystem.cpp
+++ Slice/SliceEngine/src/Graphics/LightingSystem.cpp
@@ -24,26 +24,41 @@ namespace SliceEngine
 	void LightingSystem::SetLightingParams(GLuint shader)
 	{
 		auto view = Core::GetInstance()->GetRegistry().view<lightingEntity>();
-		int i{};
+		int i{1};
+		bool hasDirLight {false};
 		std::stringstream ss{};
+		GLint uniformLoc;
 		for (auto entity : view)
 		{
 			auto& light = Core::GetInstance()->GetRegistry().get<Light>(entity);
 			auto& transform = Core::GetInstance()->GetRegistry().get<Transform>(entity);
 
-			GLint uniformLoc;
+			int lightNum = i;
+			if (light.type == Light::LightType::Directional)
+			{
+				lightNum = 0;
+				hasDirLight = true;
+			}
+
 			ss.str("");
-			ss << "uLight[" << i << "].position";
+			ss << "uLight[" << lightNum << "].position";
 			uniformLoc = glGetUniformLocation(shader, ss.str().c_str());
 			glUniform3f(uniformLoc, transform.position.x, transform.position.y, transform.position.z);
 
 			ss.str("");
-			ss << "uLight[" << i << "].color";
+			ss << "uLight[" << lightNum << "].color";
 			uniformLoc = glGetUniformLocation(shader, ss.str().c_str());
 			glm::vec3 col = light.color * light.intensity;
 			glUniform3f(uniformLoc, col.r, col.g, col.b);
 
-			++i;
+			if (i++ > 32)
+				break;
 		}
+		uniformLoc = glGetUniformLocation(shader, "numLights");
+		if(hasDirLight)
+			glUniform1i(uniformLoc, i - 1);
+		else
+			glUniform1i(uniformLoc, i);
+
 	}
 }
