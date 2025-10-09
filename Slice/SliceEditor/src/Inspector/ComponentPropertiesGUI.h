5811df9e3e504da5859d5602e444252029a59ad8
nic <nicklaiabc123@gmail.com>
2025-10-09 21:53:44 +0800

    Re-arranged things just so i can read it better

--- Slice/SliceEditor/src/Inspector/ComponentPropertiesGUI.h
+++ Slice/SliceEditor/src/Inspector/ComponentPropertiesGUI.h
@@ -20,9 +20,11 @@ DigiPen Institute of Technology is prohibited.
 namespace SliceEditor
 {
 	class Registry;
-	//void IntInput(const char* id, int& val, std::function<void(int)> setFunc = nullptr);
-	//void DragIntInput(const char* id, int& val, int min, int max, std::function<void(int)> setFunc = nullptr);
 	bool DragFloatInput(Registry& reg, const char* id, float& val, const char* format, std::function<void(float)> setFunc = nullptr, float min = 0.f, float max = 0.f);
+	bool DragVec3InputHeader(Registry& reg, const char* property_label, const char* id, glm::vec3& vec);
+
+	/*void IntInput(const char* id, int& val, std::function<void(int)> setFunc = nullptr);
+	//void DragIntInput(const char* id, int& val, int min, int max, std::function<void(int)> setFunc = nullptr);
 	//void DragDoubleInput(const char* id, double& val, const char* format, std::function<void(double)> setFunc = nullptr);
 	//void StringInput(const char* id, std::string& val, std::function<void(std::string)> setFunc = nullptr);
 	//void IntInputHeader(const char* property_label, const char* id, int& val, std::function<void(int)> setFunc = nullptr);
@@ -30,10 +32,10 @@ namespace SliceEditor
 	//void DragFloatInputHeader(const char* property_label, const char* id, float& val, const char* format = "%.3f", std::function<void(float)> setFunc = nullptr, float min = 0.f, float max = 0.f);
 	//void DragDoubleInputHeader(const char* property_label, const char* id, double& val, const char* format = "%.3f", std::function<void(double)> setFunc = nullptr);
 	//void DragVec2InputHeader(const char* property_label, const char* id, MathLib::vec2& val);
-	bool DragVec3InputHeader(Registry& reg, const char* property_label, const char* id, glm::vec3& vec);
 	//void DragColorInputHeader(const char* property_label, const char* id, glm::vec4& color);
 	//void StringInputHeader(const char* property_label, const char* id, std::string& val);
 	//void AssetDragDropInputHeader(const char* property_label, const char* id, std::string& val, std::function<void(std::string)> setFunc, const char* asset_type);
+	*/
 
 	template <typename T, typename Container>
 	void ComboHeader(const char* property_label, std::string& selected, Container& container, std::function<std::string(T&)>& iterateStringFunc)
