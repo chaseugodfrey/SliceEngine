b096cb992620eda70ed638c6593d35abd3b1bb2e
Hafiz <hafionn@gmail.com>
2025-10-31 12:12:41 +0800

    comment out transforms

--- Slice/SliceScript/Source/TestMovement.cs
+++ Slice/SliceScript/Source/TestMovement.cs
@@ -11,20 +11,20 @@ namespace SliceEngine
         public float speed = 5.0f;
         public Vector3 direction = new Vector3(-1.0f, 0.0f, 0.0f);
 
-        public Transform t;
+        //public Transform t;
         
 
         public override void OnCreate()
         {
             Console.WriteLine("Test Movement onCreate");
-            t = GetComponent<Transform>();           
+            //t = GetComponent<Transform>();           
         }
 
         public override void OnUpdate(float dt)
         {
             // This part is not even running
             Console.WriteLine("I'm running btw");
-            t.Position = new Vector3(t.Position.x + speed*dt, 0, 0);
+            //t.Position = new Vector3(t.Position.x + speed*dt, 0, 0);
         }
 
     }
