34bf81842a745148d22054235717f0c27523af77
Muhammad Hafiz Bin Onn <b.muhammadhafiz@digipen.edu>
2025-11-08 20:37:18 +0800

    nothing

--- Slice/SliceEngine/src/Systems/SceneSystem.h
+++ Slice/SliceEngine/src/Systems/SceneSystem.h
@@ -71,7 +71,7 @@ namespace SliceEngine
 		//can change to other identifier
 
 		std::queue<std::filesystem::path> mSceneQueue;
-		std::filesystem::path mCurrentScene{};
+		std::filesystem::path mCurrentScene{};	
 		std::filesystem::path mNextScene{};
 		std::filesystem::path mDefaultScene{};
 		
