b272abf80e7006071df9fb21e0552511a3c8b082
lyongyee <69913917+lyongyee@users.noreply.github.com>
2025-11-09 19:18:31 +0800

    made file watch ignore texture, model , material and controller files

--- Slice/SliceEditor/src/AssetManager/AssetManager.cpp
+++ Slice/SliceEditor/src/AssetManager/AssetManager.cpp
@@ -882,9 +882,15 @@ namespace SliceEditor
 
 	void AssetManager::HandleAssetAdded(RawFileEvent& addEvent)
 	{
-		if (addEvent.filePath.extension() == ".mat" || addEvent.filePath.extension() == ".controller")
+		for (auto& [key, value] : mSupportedAssetTypes)
 		{
-			return;
+			if (addEvent.filePath.extension() == key)
+			{
+				if (value.first == AssetType::Texture || value.first == AssetType::Model || value.first == AssetType::Controller || value.first == AssetType::Material)
+				{
+					return;
+				}
+			}
 		}
 
 		CreateDescriptorFile(addEvent.filePath);
