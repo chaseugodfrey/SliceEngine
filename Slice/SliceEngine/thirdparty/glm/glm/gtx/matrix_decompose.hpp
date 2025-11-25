db8687ecf290ba2b0e15205b16058ce244f7da8b
AloysiusTeo <teo.k@digipen.edu>
2025-11-25 22:03:57 +0800

    syncing of transform and physics bodies fixed

--- Slice/SliceEngine/thirdparty/glm/glm/gtx/matrix_decompose.hpp
+++ Slice/SliceEngine/thirdparty/glm/glm/gtx/matrix_decompose.hpp
@@ -20,11 +20,8 @@
 #include "../gtc/quaternion.hpp"
 #include "../gtc/matrix_transform.hpp"
 
-#ifndef GLM_ENABLE_EXPERIMENTAL
-#	error "GLM: GLM_GTX_matrix_decompose is an experimental extension and may change in the future. Use #define GLM_ENABLE_EXPERIMENTAL before including it, if you really want to use it."
-#elif GLM_MESSAGES == GLM_ENABLE && !defined(GLM_EXT_INCLUDED)
-#	pragma message("GLM: GLM_GTX_matrix_decompose extension included")
-#endif
+#define GLM_ENABLE_EXPERIMENTAL
+
 
 namespace glm
 {
