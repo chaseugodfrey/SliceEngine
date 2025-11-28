1ee6eb2f33c7c32753a3d4b991bdf279a3e6eac4
Muhammad Hafiz Bin Onn <b.muhammadhafiz@digipen.edu>
2025-11-28 21:50:16 +0800

    ??? works

--- Slice/SliceScript/Source/Examples/TestMisc.cs
+++ Slice/SliceScript/Source/Examples/TestMisc.cs
@@ -10,18 +10,21 @@ namespace SliceEngine
     {
         public override void OnCreate()
         {
-            var objs = FindGameObjectsWithTag("A Tag That Will Never Exist");
-            if (objs.Length == 0)
-            {
-                SliceLog.Log("FindGameObjectsWithTag in TestMisc script successfully returned null");
-            }
+            //var objs = FindGameObjectsWithTag("A Tag That Will Never Exist");
+            //if (objs is null)
+            //{
+            //    SliceLog.Log("FindGameObjectsWithTag in TestMisc script successfully returned null");
+            //}
 
-            var obj = FindGameObjectWithName("A Name That Will Never Exist");
-            if (obj is null)
-            {
-                SliceLog.Log("FindGameObjectWithName in TestMisc script successfully returned null");
-            }
+            //var obj = FindGameObjectWithName("A Name That Will Never Exist");
+            //if (obj is null)
+            //{
+            //    SliceLog.Log("FindGameObjectWithName in TestMisc script successfully returned null");
+            //}
 
+            //Heresy?
+            FindGameObjectWithName("A Name That Will Never Exist")?.GetComponent<RigidBody>()?.AddForce(Vector3.Zero);
+            FindGameObjectsWithTag("A Tag That Will Never Exist")?[0]?.GetComponent<RigidBody>()?.AddForce(Vector3.Zero);
         }
     }
 }
