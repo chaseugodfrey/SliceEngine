3da23bddc4c08deafc0f30e4eddc3498aad8e259
Elton <leosantosa.elton@gmail.com>
2025-11-29 21:01:22 +0800

    fix texture compiler

--- Temporary_Holding_Location/Compiler_Proj/TextureCompile/TextureLoader.cpp
+++ Temporary_Holding_Location/Compiler_Proj/TextureCompile/TextureLoader.cpp
@@ -15,10 +15,53 @@ namespace Texture_Asset{
 
 	void Texture_Compiler::Read_Json(nlohmann::json const& obj) {
 		//parse meta file for the compiling options
-		if (obj.contains("")) {
-
+		if (obj.contains("comp_quality")) {
+			comp_quality = obj["comp_quality"];
 		}
-
+		if (obj.contains("generateMips")) {
+			generate_mips = obj["generateMips"];
+		}
+		if (obj.contains("mip_count")) {
+			mip_count = obj["mip_count"];
+		}
+		if (obj.contains("comp_format")) {
+			CompressionFormat format = obj["comp_format"];
+			switch (format) {
+			case BC1:
+				cmp_format = CMP_FORMAT_BC1;
+				break;
+			case BC2:
+				cmp_format = CMP_FORMAT_BC2;
+				break;
+			case BC3:
+				cmp_format = CMP_FORMAT_BC3;
+				break;
+			case BC4:
+				cmp_format = CMP_FORMAT_BC4;
+				break;
+			case BC4s:
+				cmp_format = CMP_FORMAT_BC4_S;
+				break;
+			case BC5:
+				cmp_format = CMP_FORMAT_BC5;
+				break;
+			case BC5s:
+				cmp_format = CMP_FORMAT_BC5_S;
+				break;
+			case BC6:
+				cmp_format = CMP_FORMAT_BC6H;
+				break;
+			case BC6s:
+				cmp_format = CMP_FORMAT_BC6H_SF;
+				break;
+			case BC7:
+				cmp_format = CMP_FORMAT_BC7;
+				break;
+			default:
+				cmp_format = CMP_FORMAT_BC7;
+				break;
+			}
+		}
 	}
 
 	bool Texture_Compiler::Compile_Asset(const char* src, const char* dest) {
