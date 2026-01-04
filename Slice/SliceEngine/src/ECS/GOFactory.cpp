a9503abc13189f39f1b94eced68c5a4d2ebd29e6
nic <nicklaiabc123@gmail.com>
2026-01-04 22:00:20 +0800

    Fixed the SetParenting bugs

--- Slice/SliceEngine/src/ECS/GOFactory.cpp
+++ Slice/SliceEngine/src/ECS/GOFactory.cpp
@@ -340,9 +340,15 @@ namespace SliceEngine
 			return false;
 		}
 
+
 		auto& scene_graph = mRegistry.get<SceneGraph>(entity);
 		auto prev_parent_entity = scene_graph.neighbours[SceneGraph::UP];
-
+		//Check if there's even a need to update the parent.
+		if (prev_parent_entity == parentEntity)
+		{
+			SLICE_LOG_WARNING("Parenting to self. Does nothing.");
+			return false;
+		}
 		// if the base entity has a parent, then we want to unattach it from its current chain
 		if (prev_parent_entity != entt::null)
 		{
