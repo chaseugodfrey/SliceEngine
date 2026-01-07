5fa7668726fab94d2efe0beba301a102c23d2acb
AloysiusTeo <teo.k@digipen.edu>
2026-01-07 15:03:48 +0800

    Physics api for raycasting

---
+++ Slice/SliceScript/Source/Core/Components/Physics.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SliceEngine
+{
+    public struct Physics
+    {
+        
+        public bool RayCast()
+        {
+            return true;
+        }
+
+        public bool SphereCast()
+        {
+            return true;
+        }
+    }
+}
