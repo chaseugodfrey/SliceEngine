52ec9cf7c0beac7b642f72c8bc8ac962a1c0cf18
nic <nicklaiabc123@gmail.com>
2026-01-08 02:04:15 +0800

    disabled the getname crash for GO

--- Slice/SliceEditor/src/Inspector/ComponentPropertiesGUI.cpp
+++ Slice/SliceEditor/src/Inspector/ComponentPropertiesGUI.cpp
@@ -451,7 +451,8 @@ namespace SliceEditor
 		}
 		else
 		{
-			std::string goName = val.GetName();
+			
+			std::string goName = "val.GetName()";
 			ImGui::InputText(id, &goName);
 		}
 		ImGui::EndDisabled();
