e94f1e182dad134435acf666d2aa780dedb90258
nic <nicklaiabc123@gmail.com>
2026-01-09 17:00:01 +0800

    Goodbye tempMerge

--- Slice/SliceEngine/src/Serializer/JSONSerializer.cpp
+++ Slice/SliceEngine/src/Serializer/JSONSerializer.cpp
@@ -11,6 +11,7 @@ DigiPen Institute of Technology is prohibited.
 #include <pch.h>
 #include "JSONSerializer.h"
 #include "Animator/BoneSystem.h"
+#include "Scripting/ScriptSystem.h"
 
 namespace SliceEngine
 {
@@ -600,7 +601,8 @@ namespace SliceEngine
 						ColliderShape::BoxData,
 						ColliderShape::SphereData,
 						ColliderShape::CapsuleData,
-						RigidBody::FreezeOptions
+						RigidBody::FreezeOptions,
+						GameObject
 						>
 						(output, name, storage.type().name(), propName, propVal, static_cast<Entity>(entity));
 				}
@@ -663,7 +665,8 @@ namespace SliceEngine
 							ColliderShape::BoxData,
 							ColliderShape::SphereData,
 							ColliderShape::CapsuleData,
-							RigidBody::FreezeOptions
+							RigidBody::FreezeOptions,
+							GameObject
 							>
 							(output, name, componentType.get_name().to_string(), propName, propVal, static_cast<Entity>(entity));
 					}
@@ -833,6 +836,9 @@ namespace SliceEngine
 				slider.fill = (Entity)sceneGraphMap[(uint32_t)slider.fill];
 				slider.handle = (Entity)sceneGraphMap[(uint32_t)slider.handle];
 			}
+
+			gScriptSystem->RemapGameObjectVariables(sceneGraphMap);
+
 			// Using scene graph map to fix scenegraph component is done in another function in scene system.
 
 			return sceneGraphMap;
@@ -878,6 +884,11 @@ namespace SliceEngine
 			if (t == rttr::type::get<unsigned int>()) { return v.get_value<unsigned int>(); }
 			if (t == rttr::type::get<short>()) { return v.get_value<short>(); }
 			if (t == rttr::type::get<std::string>()) { return v.get_value<std::string>(); }
+			if (t == rttr::type::get<GameObject>()) 
+			{ 
+				Entity testVal = v.get_value<GameObject>().GetEntity();
+				return v.get_value<GameObject>().GetEntity();
+			}
 
 			// fall back is to return as a string
 			return v.to_string();
