2681756582d7602b7d38969874b773bcaefb8555
AloysiusTeo <teo.k@digipen.edu>
2026-01-10 19:28:31 +0800

    wip

--- Slice/SliceEngine/src/Physics/ContactListener.cpp
+++ Slice/SliceEngine/src/Physics/ContactListener.cpp
@@ -2,11 +2,20 @@
 #include "ContactListener.h"
 #include "../ECS/GOFactory.h"
 #include "../Core/Core.h"
+#include "../Systems/FramerateManager.h"
 
 //JPH::ValidateResult SliceEngine::MyContactListener::OnContactValidate(const JPH::Body& inBody1, const JPH::Body& inBody2, JPH::RVec3Arg inBaseOffset, const JPH::CollideShapeResult& inCollisionResult)
 //{
 //	return JPH::ValidateResult();
 //}
+
+//static timer for on contact persist need to figure time to publish event everytime
+//first add put it in the container and publish event
+//check if its in the container before adding, if in container send an event every few second time has yet to be decided
+//when a pair is added to on contact remove remove it from on contact persist
+//set time to 0 when nothing is in the container or when timer hits >0.5f
+namespace SliceEngine
+{ 
 
 void SliceEngine::MyContactListener::OnContactAdded(const JPH::Body& inBody1, const JPH::Body& inBody2, const JPH::ContactManifold& inManifold, JPH::ContactSettings& ioSettings)
 {
@@ -66,52 +75,88 @@ void SliceEngine::MyContactListener::OnContactPersisted(const JPH::Body& inBody1
 	GameObject checkEntity1 = Core::GetInstance()->mFactory.GetGOByEntity(static_cast<Entity>(inBody1.GetUserData()));
 	GameObject checkEntity2 = Core::GetInstance()->mFactory.GetGOByEntity(static_cast<Entity>(inBody2.GetUserData()));
 
-	auto& colliderShape1 = checkEntity1.GetComponent<ColliderShape>();
-	auto& colliderShape2 = checkEntity2.GetComponent<ColliderShape>();
+	timer = collisionPairs.size() == 0 ? 0.0f : timer + Core::GetInstance()->GetFramerateManager()->getFixedDeltaTime();
 
-	if (colliderShape1.isTrigger || colliderShape2.isTrigger)
+	std::pair<GameObject, GameObject> orderedPair = MakeOrderedPair(checkEntity1, checkEntity2);
+	collisionPairs.insert(orderedPair);
+
+	if(timer >= timeBetweenEvents)
 	{
-		OnTriggerStayEvent triggerEvent1;
-		OnTriggerStayEvent triggerEvent2;
-
-		triggerEvent1.entity = checkEntity1.GetEntity();
-		triggerEvent1.other = checkEntity2.GetEntity();
-
-		triggerEvent2.entity = checkEntity2.GetEntity();
-		triggerEvent2.other = checkEntity1.GetEntity();
-
-		EventManager::GetInstance()->Publish<OnTriggerStayEvent>(triggerEvent1);
-		EventManager::GetInstance()->Publish<OnTriggerStayEvent>(triggerEvent2);
-	}
-	else
-	{
-		OnCollisionStayEvent collisionEvent1;
-		OnCollisionStayEvent collisionEvent2;
-
-		collisionEvent1.entity = checkEntity1.GetEntity();
-		collisionEvent1.other = checkEntity2.GetEntity();
-
-		collisionEvent2.entity = checkEntity2.GetEntity();
-		collisionEvent2.other = checkEntity1.GetEntity();
-
-		EventManager::GetInstance()->Publish<OnCollisionStayEvent>(collisionEvent1);
-		EventManager::GetInstance()->Publish<OnCollisionStayEvent>(collisionEvent2);
-
+		PublishCollisionPersistEvents();
+		timer = 0.0f;
 	}
 
 }
 
-void SliceEngine::MyContactListener::OnContactRemoved(const JPH::SubShapeIDPair& inSubShapePair)
-{
-	bodiesInContact.emplace_back(inSubShapePair);
+	void SliceEngine::MyContactListener::OnContactRemoved(const JPH::SubShapeIDPair & inSubShapePair)
+	{
+		bodiesInContact.emplace_back(inSubShapePair);
+	}
+
+	std::vector<JPH::SubShapeIDPair> SliceEngine::MyContactListener::GetBodiesInContact() const
+	{
+		return bodiesInContact;
+	}
+
+	void SliceEngine::MyContactListener::clearCollisionsPairs()
+	{
+		collisionPairs.clear();
+	}
+
+	std::set<std::pair<GameObject, GameObject>> MyContactListener::GetCollisionPairs() const
+	{
+		return collisionPairs;
+	}
+
+	std::pair<GameObject, GameObject> MyContactListener::MakeOrderedPair(const GameObject & ent1, const GameObject & ent2)
+	{
+		return (ent1 < ent2) ? std::make_pair(ent1, ent2) : std::make_pair(ent2, ent1);
+	}
+
+	void MyContactListener::PublishCollisionPersistEvents()
+	{
+		for(const auto& pair : collisionPairs)
+		{
+
+			auto& colliderShape1 = pair.first.GetComponent<ColliderShape>();
+			auto& colliderShape2 = pair.second.GetComponent<ColliderShape>();
+
+			if (colliderShape1.isTrigger || colliderShape2.isTrigger)
+			{
+				OnTriggerStayEvent triggerEvent1;
+				OnTriggerStayEvent triggerEvent2;
+
+				triggerEvent1.entity = pair.first.GetEntity();
+				triggerEvent1.other = pair.second.GetEntity();
+
+				triggerEvent2.entity = pair.second.GetEntity();
+				triggerEvent2.other = pair.first.GetEntity();
+
+				EventManager::GetInstance()->Publish<OnTriggerStayEvent>(triggerEvent1);
+				EventManager::GetInstance()->Publish<OnTriggerStayEvent>(triggerEvent2);
+			}
+			else
+			{
+				OnCollisionStayEvent collisionEvent1;
+				OnCollisionStayEvent collisionEvent2;
+
+				collisionEvent1.entity = pair.first.GetEntity();
+				collisionEvent1.other = pair.second.GetEntity();
+
+				collisionEvent2.entity = pair.second.GetEntity();
+				collisionEvent2.other = pair.first.GetEntity();
+
+				EventManager::GetInstance()->Publish<OnCollisionStayEvent>(collisionEvent1);
+				EventManager::GetInstance()->Publish<OnCollisionStayEvent>(collisionEvent2);
+
+			}
+		}
+	}
+
+	void SliceEngine::MyContactListener::clearBodiesInContact()
+	{
+		bodiesInContact.clear();
+	}
+
 }
 
-std::vector<JPH::SubShapeIDPair> SliceEngine::MyContactListener::GetBodiesInContact() const
-{
-	 return bodiesInContact; 
-}
-
-void SliceEngine::MyContactListener::clearBodiesInContact()
-{
-	bodiesInContact.clear();
-}
