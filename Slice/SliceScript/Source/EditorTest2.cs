c7298b1df20464be43f41d691186879e5dfe2e17
nic <nicklaiabc123@gmail.com>
2026-01-14 05:17:58 +0800

    since i cant sleep might as well work on GAM

---
+++ Slice/SliceScript/Source/EditorTest2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SliceEngine
+{
+    public class EditorTest2 : SliceBehaviour
+    {
+        public bool boolTest2 = false;
+        public GameObject GOtest2;
+        public Vector3 vec3test2;
+        public override void OnUpdate(float dt)
+        {
+
+            //GOtest2 = FindGameObjectWithName("");
+
+            //SliceLog.Log("X: " + vec3test.x + " Y: " + vec3test.y + " Z: " + vec3test.z);
+        }
+    }
+}
