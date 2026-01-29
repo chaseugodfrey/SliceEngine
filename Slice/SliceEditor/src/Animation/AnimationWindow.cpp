870bb29ee6694f5e99ef56f97670403c41400ca6
rayman-png <143987713+rayman-png@users.noreply.github.com>
2026-01-29 20:04:39 +0800

    fix armor not animated

--- Slice/SliceEditor/src/Animation/AnimationWindow.cpp
+++ Slice/SliceEditor/src/Animation/AnimationWindow.cpp
@@ -205,34 +205,10 @@ namespace SliceEditor
 	{
 		auto core = SliceEngine::Core::GetInstance();
 
-		auto const& bone = core->GetRegistry().try_get<SliceEngine::Bone>(ent);
-		if(bone)
+		auto const& cAnimator = core->GetRegistry().try_get<SliceEngine::Animator>(ent);
+
+		if (cAnimator)
 		{
-			Entity root_entity = bone->skeleton_root;
-			if (root_entity != ent)
-			{
-				//auto& animator = core->GetRegistry().get<SliceEngine::Animator>(root_entity);
-				auto& transform = core->GetRegistry().get<SliceEngine::Transform>(ent);
-
-				//if (!mTimeline.isPlaying)
-					///continue;
-
-				//some pseudo code
-				glm::mat4 const& frame = mCurrentAnimator->GetFinalTform()[bone->frame_idx];
-				glm::vec3 translation, scale, skew;
-				glm::vec4 perspective;
-				glm::quat rotation;
-				glm::decompose(frame, scale, rotation, translation, skew, perspective);
-				transform.position = translation;
-				transform.rotation = rotation;
-				transform.scale = scale;
-
-				//if is a renderer, tell skeleton to calculate inverse for this index
-				if (core->GetRegistry().any_of<SliceEngine::Renderer>(ent)) {
-					mCurrentAnimator->inverse_flags.set(bone->frame_idx);
-				}
-			}
-
 			if (auto scene_graph = core->GetRegistry().try_get<SliceEngine::SceneGraph>(ent)) {
 				entt::entity child = scene_graph->neighbours[SliceEngine::SceneGraph::DOWN];
 				while (child != entt::null)
@@ -242,10 +218,46 @@ namespace SliceEditor
 				}
 			}
 		}
+		else
+		{
+			auto const& bone = core->GetRegistry().try_get<SliceEngine::Bone>(ent);
+			if (bone)
+			{
+				Entity root_entity = bone->skeleton_root;
+				if (root_entity != ent)
+				{
+					//auto& animator = core->GetRegistry().get<SliceEngine::Animator>(root_entity);
+					auto& transform = core->GetRegistry().get<SliceEngine::Transform>(ent);
 
+					//if (!mTimeline.isPlaying)
+						///continue;
 
+					//some pseudo code
+					glm::mat4 const& frame = mCurrentAnimator->GetFinalTform()[bone->frame_idx];
+					glm::vec3 translation, scale, skew;
+					glm::vec4 perspective;
+					glm::quat rotation;
+					glm::decompose(frame, scale, rotation, translation, skew, perspective);
+					transform.position = translation;
+					transform.rotation = rotation;
+					transform.scale = scale;
 
-		
+					//if is a renderer, tell skeleton to calculate inverse for this index
+					if (core->GetRegistry().any_of<SliceEngine::Renderer>(ent)) {
+						mCurrentAnimator->inverse_flags.set(bone->frame_idx);
+					}
+				}
+
+				if (auto scene_graph = core->GetRegistry().try_get<SliceEngine::SceneGraph>(ent)) {
+					entt::entity child = scene_graph->neighbours[SliceEngine::SceneGraph::DOWN];
+					while (child != entt::null)
+					{
+						UpdateBoneScene(child);
+						child = core->GetRegistry().get<SliceEngine::SceneGraph>(child).neighbours[SliceEngine::SceneGraph::RIGHT];
+					}
+				}
+			}
+		}
 	}
 
 	void AnimationWindow::UpdateBones()
