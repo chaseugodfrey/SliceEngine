42d1e45d0b134cabae5238157186059cf399cace
chaseugodfrey <143150195+chaseugodfrey@users.noreply.github.com>
2026-02-02 00:27:40 +0800

    Added Build Settings
    - need to remove scene system editor only loading
    - need to add loading by int in scene system

--- Slice/SliceEngine/src/Systems/SceneSystem.h
+++ Slice/SliceEngine/src/Systems/SceneSystem.h
@@ -46,7 +46,6 @@ namespace SliceEngine
 		void LoadSceneIntoQueue(std::filesystem::path const filePath);
 		bool LoadSceneFromQueue();
 		std::string LoadNavMeshFromMeta(std::filesystem::path navMeshFile);
-		void LoadNextScene();
 		void WriteTempFile();
 		
 		void OnSceneSave(std::filesystem::path const filePath);
@@ -73,6 +72,7 @@ namespace SliceEngine
 		std::queue<std::filesystem::path> mSceneQueue;
 		std::filesystem::path mCurrentScene{};
 		std::filesystem::path mNextScene{};
+		GUID mCurrentSceneGUID;
 		
 		std::string mCurrentSceneName{};
 	};
