36f19c7df723393deac3d2c9a4ce16310844f644
nic <nicklaiabc123@gmail.com>
2026-02-04 15:23:41 +0800

    NavObstacle Inspector

--- Slice/SliceEditor/src/Inspector/InspectorWindow.cpp
+++ Slice/SliceEditor/src/Inspector/InspectorWindow.cpp
@@ -661,8 +661,6 @@ namespace SliceEditor
 
 	void InspectorWindow::DisplayNavMeshLink(entt::entity entity)
 	{
-		auto& agent = SliceEngine::Core::GetInstance()->GetRegistry().get<SliceEngine::NavMeshLink>(entity);
-
 		if (ImGui::TreeNodeEx("Nav Mesh Link", mBaseFlags))
 		{
 			auto& navLink = SliceEngine::Core::GetInstance()->GetRegistry().get<SliceEngine::NavMeshLink>(entity);
@@ -677,6 +675,19 @@ namespace SliceEditor
 
 			//ImGui::TreePop();
 		}
+
+	void InspectorWindow::DisplayNavObstacle(entt::entity entity)
+	{
+		auto& navObstacle = SliceEngine::Core::GetInstance()->GetRegistry().get<SliceEngine::NavObstacle>(entity);
+
+		if (ImGui::TreeNodeEx("Nav Obstacle", mBaseFlags))
+		{
+			DisplayComponentHeader<SliceEngine::NavObstacle>(entity);
+
+			BoolInputHeader(mRegistry, "Is Obstacle: ", "##isNavObstacle", navObstacle.isObstacle);
+			ImGui::TreePop();
+		}
+	}
 
 	void InspectorWindow::DisplaySliceScript(entt::entity entity)
 	{
@@ -1568,6 +1579,14 @@ namespace SliceEditor
 				}
 			}
 
+			if (!selectedGO.HasComponent<SliceEngine::NavObstacle>())
+			{
+				if (ImGui::Selectable("Add Nav Obstacle"))
+				{
+					reg.emplace<SliceEngine::NavObstacle>(entity);
+				}
+			}
+
 			if(!selectedGO.HasComponent<SliceEngine::ColliderShape>())
 			{
 				if (ImGui::Selectable("Add Box Collider"))
@@ -1766,6 +1785,12 @@ namespace SliceEditor
 			if (SliceEngine::Core::GetInstance()->GetRegistry().try_get<SliceEngine::NavMeshLink>(entity))
 			{
 				DisplayNavMeshLink(node->entity);
+				ImGui::Separator();
+			}
+
+			if (SliceEngine::Core::GetInstance()->GetRegistry().try_get<SliceEngine::NavObstacle>(entity))
+			{
+				DisplayNavObstacle(node->entity);
 				ImGui::Separator();
 			}
 
