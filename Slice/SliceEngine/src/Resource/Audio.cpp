7f306bd29e08a22a520ea874e1594fbc2e357588
YY <lyongyee.official@gmail.com>
2026-02-04 23:51:11 +0800

    i pay again

--- Slice/SliceEngine/src/Resource/Audio.cpp
+++ Slice/SliceEngine/src/Resource/Audio.cpp
@@ -73,9 +73,11 @@ namespace SliceEngine
                                 
                                 SLICE_LOG_WARNING("JSON Error in meta file, falling back to heuristic: " + metaPath.string());
                             }
+
+                            metaFile.close();
                         }
 
-                        metaFile.close();
+
                         break;
                     }
                 }
