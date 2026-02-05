692e133b741f02a4bcf78d02d03da8cc97c95110
lyongyee <69913917+lyongyee@users.noreply.github.com>
2026-02-05 22:05:30 +0800

    is playing

--- Slice/SliceEngine/src/Engine.cpp
+++ Slice/SliceEngine/src/Engine.cpp
@@ -762,6 +762,12 @@ namespace SliceEngine
 		auto& prefabSys = core->GetSystem<PrefabSystem>();
 		auto& sParticleSystemManager = core->GetSystem<ParticleSystemManager>();
 
+<<<<<<< Updated upstream
+=======
+		
+
+		//static bool isPlaying = false;
+>>>>>>> Stashed changes
 		//
 
 		//frm->StartFrame();
