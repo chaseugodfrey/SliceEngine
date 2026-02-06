586a3811278cdb2c441fa0c2cc30f3a66ff27482
zekingfx <zekingfx@gmail.com>
2026-02-06 13:12:42 +0800

    a

--- Slice/SliceScript/Source/Core/Enemy/EnemyLevels/BaseLevel.cs
+++ Slice/SliceScript/Source/Core/Enemy/EnemyLevels/BaseLevel.cs
@@ -65,21 +65,21 @@ namespace SliceEngine
                 toggleLevel = true;
             }
 
-            SliceLog.Log("Base Level passed check objective");
+            //SliceLog.Log("Base Level passed check objective");
 
             if (toggleLevel)
             {
                 return;
             }
 
-            SliceLog.Log("Base Level passed toggle level");
+            //SliceLog.Log("Base Level passed toggle level");
 
             if (toggleSpawning)
             {
                 timer += dt;
             }
 
-            SliceLog.Log("Base Level passed toggle spawning");
+            //SliceLog.Log("Base Level passed toggle spawning");
 
             // when it reaches the last enemy point
             if (currPoint >= enemyPoints.Count)
@@ -93,7 +93,7 @@ namespace SliceEngine
                 return;
             }
 
-            SliceLog.Log("Base Level passed curr");
+            //SliceLog.Log("Base Level passed curr");
 
             if (levelDirectorObject == null)
             {
@@ -101,7 +101,7 @@ namespace SliceEngine
             }
             //SliceLog.Log("Curr time : " + timer);
 
-            SliceLog.Log("Base Level passed ;vl dri object");
+            //SliceLog.Log("Base Level passed ;vl dri object");
 
             if (toggleSpawning && timer > spawnInterval)
             {
@@ -109,7 +109,7 @@ namespace SliceEngine
                 Transform pointTransform = enemyPoints[currPoint].GetComponent<Transform>();
                 if (!levelDirectorObject.As<LevelDirector>().CanCreateEnemy(pointTransform.WorldPosition))
                 {
-                    SliceLog.Log("Died in here 3");
+                    //SliceLog.Log("Died in here 3");
                     // if the curr point can't then just go next point instead
                     timer = 0.0f;
                     currPoint++;
@@ -131,7 +131,7 @@ namespace SliceEngine
 
             }
 
-            SliceLog.Log("Base Level should be working");
+            //SliceLog.Log("Base Level should be working");
 
         }
 
