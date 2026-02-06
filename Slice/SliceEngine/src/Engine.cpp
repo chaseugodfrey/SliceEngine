fe5990f7c1f4cdf387187bdec19a43314ae49d1b
NickyWallnut <nicklaiabc123@gmail.com>
2026-02-06 13:48:50 +0800

    Fixed Play/Stop for Profiler
    why tf play/stop keep dying wth

--- Slice/SliceEngine/src/Engine.cpp
+++ Slice/SliceEngine/src/Engine.cpp
@@ -776,7 +776,6 @@ namespace SliceEngine
 
 		//frm->StartFrame();
 
-		frm->StartSystem("Scene Handling");
 		if (!sScene->CheckQueueEmpty())
 		{
 			if (sScene->isSceneUnloaded)
@@ -846,7 +845,6 @@ namespace SliceEngine
 			}
 		}
 
-		frm->EndSystem("Scene Handling");
 
 		frm->StartSystem("Update Delta Time");
 		frm->updateDeltaTime(); //update deltatime and currentnumber of steps for systems that uses fixeddt
