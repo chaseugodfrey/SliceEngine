09cff0ad401953dff0318a053b926ef6d74c955e
lyongyee <69913917+lyongyee@users.noreply.github.com>
2026-02-07 08:43:21 +0800

    hehehe

---
+++ Slice/SliceScript/Source/Core/UI/SceneTransition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SliceEngine;
+
+namespace SliceEngine
+{
+    public class SceneTransition : SliceBehaviour
+    {
+        GameObject rect;
+
+        public override void OnCreate()
+        {
+            rect = FindGameObjectWithName("Transition");
+        }
+
+        public override void OnUpdate(float dt)
+        {
+            SpriteRenderer image = rect.GetComponent<SpriteRenderer>();
+            float duration = 2.0f;
+        }
+    }
+}
