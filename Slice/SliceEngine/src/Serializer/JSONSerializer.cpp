16d1e73f8d5f198ec47fe4131444540e47ebecbf
Elton <leosantosa.elton@gmail.com>
2026-02-08 01:12:21 +0800

    prefab fix for slider

--- Slice/SliceEngine/src/Serializer/JSONSerializer.cpp
+++ Slice/SliceEngine/src/Serializer/JSONSerializer.cpp
@@ -442,18 +442,19 @@ namespace SliceEngine
 				Core::GetInstance()->GetSystem<BoneSystem>().Update_Bones(registry, entity);
 			}
 
-			if (rootGO.HasComponent<Slider>()) {	//handle and fill entity remapping for slider
-				auto entityView = registry.view<Slider>();
-				for (auto entity : entityView) {
+			for (auto entity : entityID) {
+				if (registry.any_of<Slider>(entity)) {    //handle and fill entity remapping for slider
+
 					auto& slider = registry.get<Slider>(entity);
 
 					slider.fill = (Entity)sceneGraphMap[(uint32_t)slider.fill];
 					slider.handle = (Entity)sceneGraphMap[(uint32_t)slider.handle];
 				}
 			}
+			
 
 
-			return rootEntity;
+    return rootEntity;
 		}
 		std::unordered_map<unsigned int, std::vector<rttr::variant>> DeserializePrefabComponents(std::filesystem::path const& filePath)
 		{
