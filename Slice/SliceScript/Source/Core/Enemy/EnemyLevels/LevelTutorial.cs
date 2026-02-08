0c63d3a2c2d8eadd4b8e1b8338af643b15596587
Gideon Francis <gideonnfrancis999@gmail.com>
2026-02-08 20:25:59 +0800

    tutorial stuff

---
+++ Slice/SliceScript/Source/Core/Enemy/EnemyLevels/LevelTutorial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SliceEngine
+{
+    public class LevelTutorial : BaseLevel
+    {
+        public int tutorialIndex = 0;
+        public int KillsRequired = 0;
+        public override bool CheckObjective()
+        {
+            if (Bootstrap.HUDManager.As<HUD_Manager>().dialogueDone && KillsRequired == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public override void EnemyKilled(GameObject enemy)
+        {
+            base.EnemyKilled(enemy);
+
+            KillsRequired--;
+
+        }
+
+    }
+}
