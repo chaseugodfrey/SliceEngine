b26bfe24cadfcf48c1146d0fb1c9170a33f566f5
YY <lyongyee.official@gmail.com>
2026-02-08 22:55:53 +0800

    edited main for windowed app

--- WeightOfTheSky/WeightOfTheSky/src/main.cpp
+++ WeightOfTheSky/WeightOfTheSky/src/main.cpp
@@ -1,6 +1,10 @@
 #include <pch.h>
 #include "Application/Application.h"
 
+#ifdef _WIN32
+#pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
+#endif
+
 int main()
 {
 	SliceBuild::Application app;
