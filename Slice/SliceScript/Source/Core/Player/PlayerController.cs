19de0fe8a2e8bceeeed70b0b1fcf3faba35452b1
Gideonnf <gideonnfrancis999@gmail.com>
2026-03-03 19:05:19 +0800

    Fix jump, fallspeed and rigidbody movement

--- Slice/SliceScript/Source/Core/Player/PlayerController.cs
+++ Slice/SliceScript/Source/Core/Player/PlayerController.cs
@@ -10,7 +10,6 @@ using static SliceEngine.GeneralHitbox;
 
 namespace SliceEngine
 {
-
     public class PlayerController : Entity, IInitializable
     {
         // Debug Mode
@@ -41,7 +40,7 @@ namespace SliceEngine
         }
 
         public enum CurrentAttack
-        {            
+        {
             GroundAttack,
             PlungeLand,
             None
@@ -53,7 +52,7 @@ namespace SliceEngine
             Disabled,
             Teleporting,
             Cutscene
-        }        
+        }
 
         public MovementState playerMovementState = MovementState.Idle;
         public CombatState playerCombatState = CombatState.None;
@@ -74,7 +73,7 @@ namespace SliceEngine
         public List<float> attackDuration = new List<float>();
         public List<Vector3> attackWindows = new List<Vector3>();
         float attackTimer = 0.0f;
-        bool attackQueued;        
+        bool attackQueued;
 
         //public float attackResetTime = 1f;
         private float attackResetTimer = 0f;
@@ -99,7 +98,7 @@ namespace SliceEngine
         // Jumps
         int jumpCounter = 0;
         public int jumpMax = 2;
-        public float jumpVerticalForce = 10.0f;
+        public float jumpVerticalForce = 15.0f;
         public float jumpHorizontalForce = 7.5f;
         public float jumpDuration = 0.25f;
         public float jumpCooldown = 0.25f;
@@ -110,10 +109,11 @@ namespace SliceEngine
 
         public float fallTimeThreshold = 0.25f;
         float fallTimeTimer = 0.0f;
+        public float fallSpeed = 20.0f;
 
         // Movement
         Vector3 input;
-        Vector3 finalMove;
+        //Vector3 finalMove;
         public float moveSpeed = 2.5f;
         public float moveAcceleration = 100.0f;
         public float moveDeceleration = 100.0f;
@@ -130,6 +130,8 @@ namespace SliceEngine
         Vector3 dashDir = Vector3.Zero;
         public float iFrameDuration = 0.2f;
         public float flickerDuration = 0.05f;
+        public bool iFrames = false;
+
         public void Initialize()
         {
             camera = Bootstrap.CameraController; if (camera == null) SliceLog.Warn("PlayerController cannot find camera");
@@ -138,7 +140,7 @@ namespace SliceEngine
         }
 
         public override void OnCreate()
-        {            
+        {
             InitializeInternalReferences();
             InitializeAttacks();
         }
@@ -148,11 +150,10 @@ namespace SliceEngine
             MovementState prevMoveState = playerMovementState;
             CombatState prevCombatState = playerCombatState;
 
-            GroundCheck();
             HandleInputs();
+            GroundCheck();
 
             UpdateTimers(dt);
-            UpdateMovements(dt);
             UpdateAttacks(dt);
             UpdateStates();
             UpdateAnimator();
@@ -168,6 +169,12 @@ namespace SliceEngine
                 SliceLog.Log(playerCombatState.ToString());
                 OnCombatStateChange();
             }
+        }
+
+        public override void OnFixedUpdate(float dt)
+        {
+            UpdateMovements(dt);
+
         }
 
         void OnMovementStateChange()
@@ -199,6 +206,8 @@ namespace SliceEngine
 
             if (input.SquareMagnitude() > 1f) input = input.Normalize();
         }
+
+        #region Attacking
         private void ExecuteAttack()
         {
             if (playerCombatState != CombatState.Attacking && playerMovementState != MovementState.Plunging && grounded)
@@ -249,7 +258,7 @@ namespace SliceEngine
                 playerCombatState = CombatState.Attacking;
                 playerMovementState = MovementState.Plunging;
                 playerCurrentAttack = CurrentAttack.None;
-            }            
+            }
         }
         private void TurnOffHitboxes()
         {
@@ -292,33 +301,6 @@ namespace SliceEngine
             }
         }
 
-        //private IEnumerator iFrameAnimation(float duration)
-        //{
-        //    float timer = 0.0f;
-        //    float flickerTimer = 0.0f;
-        //    bool flicker = false;
-        //    while (timer < duration)
-        //    {
-        //        if (flickerTimer >= flickerDuration)
-        //        {
-        //            playerModel.As<PlayerAnimatorEvents>().SetModelVisible(flicker);
-        //            flicker = !flicker;
-        //            Console.WriteLine($"Flicker timer {flickerTimer}");
-        //            flickerTimer = 0.0f;
-        //        }
-
-        //        Console.WriteLine($"total timer {timer}");
-
-        //        timer += Time.deltaTime;
-        //        flickerTimer += Time.deltaTime;
-        //        yield return null;
-        //    }
-
-        //     Set visible at the end
-        //    playerModel.As<PlayerAnimatorEvents>().SetModelVisible(true);
-        //    iFrames = false;
-        //}
-
         private void Attack1(GameObject target)
         {
             EnemyBase enemy = target.As<EnemyBase>();
@@ -335,7 +317,8 @@ namespace SliceEngine
                 enemy.TakeDamage(attackDamageValues[attackCounter]);
             }
         }
-        private void Attack3(GameObject target){
+        private void Attack3(GameObject target)
+        {
             EnemyBase enemy = target.As<EnemyBase>();
             if (enemy != null)
             {
@@ -360,6 +343,35 @@ namespace SliceEngine
             playerMovementState = grounded ? MovementState.Idle : MovementState.Falling;
 
         }
+
+        private void TryAttack()
+        {
+            attackQueued = true;
+        }
+
+        private void InitializeAttacks()
+        {
+            attackHitboxes.Clear();
+            attackHitboxes.Add(FindGameObjectWithName(attackHitboxNames[0])?.As<GeneralHitbox>());
+            attackHitboxes[0].HitBoxListeners += Attack1;
+
+            attackHitboxes.Add(FindGameObjectWithName(attackHitboxNames[1])?.As<GeneralHitbox>());
+            attackHitboxes[1].HitBoxListeners += Attack2;
+
+            attackHitboxes.Add(FindGameObjectWithName(attackHitboxNames[3])?.As<GeneralHitbox>());
+            attackHitboxes[2].HitBoxListeners += Attack3;
+
+            //TurnOffHitboxes();
+        }
+        private void InitializeInternalReferences()
+        {
+            playerModel = gameObject.FindGameObjectWithName("RootNode"); if (playerModel == null) SliceLog.Warn("PlayerController cannot find RootNode");
+            animator = playerModel?.GetComponent<Animator>(); if (playerModel == null) SliceLog.Warn("PlayerController cannot find Animator");
+            audio = gameObject.GetComponent<AudioSource>(); if (playerModel == null) SliceLog.Warn("PlayerController cannot find AudioSource");
+            rigidBody = GetComponent<RigidBody>(); if (playerModel == null) SliceLog.Warn("PlayerController cannot find RigidBody");
+        }
+
+        #endregion
 
         #region On Overrides
         protected override void OnHeal() { }
@@ -396,7 +408,7 @@ namespace SliceEngine
             jumpLandTimer = (jumpLandTimer > 0.0f) ? jumpLandTimer - dt : 0.0f;
 
             if (playerCombatState != CombatState.Attacking)
-            attackResetTimer = (attackResetTimer <= attackRecoveryDuration) ? attackResetTimer + dt : 0.0f;
+                attackResetTimer = (attackResetTimer <= attackRecoveryDuration) ? attackResetTimer + dt : 0.0f;
 
             if (!grounded && rigidBody.Velocity.y < -2f) fallTimeTimer += dt;
             else fallTimeTimer = 0.0f;
@@ -404,7 +416,7 @@ namespace SliceEngine
         private void UpdateMovements(float dt)
         {
             Vector3 camForward = new Vector3();
-            finalMove = Vector3.Zero;
+            //finalMove = Vector3.Zero;
             if (camera != null)
             {
                 camForward = camera.transform.RotationQuat * Vector3.Forward; // Get camera forward direction
@@ -448,8 +460,20 @@ namespace SliceEngine
                     transform.RotationQuat = Quaternion.Slerp(transform.RotationQuat, targetRot, scaledRotSpeed * Time.deltaTime);
                 }
 
-                finalMove = moveDirInput * movementSpeed;
-                transform.Position += finalMove * dt;
+                if (playerMovementState == MovementState.Falling || playerMovementState == MovementState.Jumping)
+                {
+                    Vector3 vel = rigidBody.Velocity;
+                    vel.y -= fallSpeed * dt;
+                    rigidBody.Velocity = vel;
+                }
+
+                Vector3 horizontal = moveDirInput * movementSpeed;
+                rigidBody.Velocity = new Vector3(horizontal.x, rigidBody.Velocity.y, horizontal.z);
+                //finalMove = moveDirInput * movementSpeed;
+                //Vector3 vel = rigidBody.Velocity;
+                //vel += moveDirInput * movementSpeed;
+                //rigidBody.Velocity = vel;
+                //transform.Position += finalMove * dt;
             }
         }
 
@@ -464,7 +488,8 @@ namespace SliceEngine
         }
 
         void UpdateStates()
-        {            
+        {
+            //Console.WriteLine($"Current movement state: {playerMovementState.ToString()}");
             switch (playerMovementState)
             {
                 case MovementState.Idle:
@@ -476,7 +501,7 @@ namespace SliceEngine
                     {
                         playerMovementState = MovementState.Walking;
                     }
-                    
+
                     break;
                 case MovementState.Walking:
                     if (!grounded && jumpDurationTimer <= 0.0f && fallTimeTimer > fallTimeThreshold)
@@ -560,7 +585,7 @@ namespace SliceEngine
 
                 default:
                     break;
-            }            
+            }
 
             switch (playerCombatState)
             {
@@ -599,6 +624,32 @@ namespace SliceEngine
             animator.SetBool("Land", false);
         }
 
+        private IEnumerator iFrameAnimation(float duration)
+        {
+            float timer = 0.0f;
+            float flickerTimer = 0.0f;
+            bool flicker = false;
+            while (timer < duration)
+            {
+                if (flickerTimer >= flickerDuration)
+                {
+                    playerModel.As<PlayerAnimatorEvents>().SetModelVisible(flicker);
+                    flicker = !flicker;
+                    Console.WriteLine($"Flicker timer {flickerTimer}");
+                    flickerTimer = 0.0f;
+                }
+
+                Console.WriteLine($"total timer {timer}");
+
+                timer += Time.deltaTime;
+                flickerTimer += Time.deltaTime;
+                yield return null;
+            }
+
+            //Set visible at the end
+            playerModel.As<PlayerAnimatorEvents>().SetModelVisible(true);
+            iFrames = false;
+        }
 
         void UpdateAnimator()
         {
@@ -614,7 +665,7 @@ namespace SliceEngine
             {
                 case MovementState.Idle:
                     if (animator.SafeToChange("Idle") && (String.Compare(animator.GetCurrAnimName(), "Idle") != 0))
-                    animator.SetBool("Idle", true);
+                        animator.SetBool("Idle", true);
                     break;
                 case MovementState.Walking:
                     if (animator.SafeToChange("Walk") && (String.Compare(animator.GetCurrAnimName(), "Walk") != 0))
@@ -627,6 +678,7 @@ namespace SliceEngine
                 case MovementState.DoubleJumping:
                     if (animator.SafeToChange("AirDashStart") && (String.Compare(animator.GetCurrAnimName(), "AirDashStart") != 0))
                         animator.SetBool("AirDashStart", true);
+                    Console.WriteLine("AirDashing now");
                     break;
                 case MovementState.Falling:
                     if (animator.SafeToChange("Fall") && (String.Compare(animator.GetCurrAnimName(), "Fall") != 0))
@@ -665,7 +717,7 @@ namespace SliceEngine
 
             switch (playerCombatState)
             {
-                case CombatState.None:                    
+                case CombatState.None:
                     break;
                 case CombatState.Attacking:
                     {
@@ -701,16 +753,16 @@ namespace SliceEngine
                     }
                     break;
                 case CombatState.Recovery:
-                        if (playerMovementState == MovementState.Lunging)
-                        {
-                           // playerMovementState
-                        }
-                        break;
+                    if (playerMovementState == MovementState.Lunging)
+                    {
+                        // playerMovementState
+                    }
+                    break;
                 case CombatState.Hitstun:
                     break;
                 default:
                     break;
-            }            
+            }
         }
 
         private void HandleMovementInputs()
@@ -745,7 +797,7 @@ namespace SliceEngine
                 jumpCounter++;
                 jumpCooldownTimer = jumpCooldown;
                 jumpDurationTimer = jumpDuration;
-
+                Console.WriteLine($"jump Counter {jumpCounter} and movementState: {playerMovementState.ToString()}");
                 if (jumpCounter == 1)
                     playerMovementState = MovementState.Jumping;
                 else
@@ -767,7 +819,7 @@ namespace SliceEngine
             Vector3 camRight = Vector3.Cross(Vector3.Up, camForward).Normalize();
             Vector3 moveDir = (camForward * input.z + camRight * input.x).Normalize();
             Vector3 jumpDir = moveDir * jumpHorizontalForce;
-            rigidBody.Velocity = new Vector3(jumpDir.x, jumpVerticalForce, jumpDir.z);            
+            rigidBody.Velocity = new Vector3(jumpDir.x, jumpVerticalForce, jumpDir.z);
         }
 
         void TryDash()
@@ -794,39 +846,14 @@ namespace SliceEngine
             }
         }
 
-        private void TryAttack()
-        {            
-            attackQueued = true;
-        }
-
-        private void InitializeAttacks()
-        {
-            attackHitboxes.Clear();
-            attackHitboxes.Add(FindGameObjectWithName(attackHitboxNames[0])?.As<GeneralHitbox>());
-            attackHitboxes[0].HitBoxListeners += Attack1;
-
-            attackHitboxes.Add(FindGameObjectWithName(attackHitboxNames[1])?.As<GeneralHitbox>());
-            attackHitboxes[1].HitBoxListeners += Attack2;
-
-            attackHitboxes.Add(FindGameObjectWithName(attackHitboxNames[3])?.As<GeneralHitbox>());
-            attackHitboxes[2].HitBoxListeners += Attack3;
-
-            //TurnOffHitboxes();
-        }
-        private void InitializeInternalReferences()
-        {
-            playerModel = gameObject.FindGameObjectWithName("RootNode"); if (playerModel == null) SliceLog.Warn("PlayerController cannot find RootNode");
-            animator = playerModel?.GetComponent<Animator>(); if (playerModel == null) SliceLog.Warn("PlayerController cannot find Animator");
-            audio = gameObject.GetComponent<AudioSource>(); if (playerModel == null) SliceLog.Warn("PlayerController cannot find AudioSource");
-            rigidBody = GetComponent<RigidBody>(); if (playerModel == null) SliceLog.Warn("PlayerController cannot find RigidBody");            
-        }
-
         private void GroundCheck()
         {
             grounded = (groundContactCount > 0); ;
             if (grounded)
             {
-                jumpCounter = 0;
+                if (playerMovementState != MovementState.Jumping && playerMovementState != MovementState.Falling)
+                    jumpCounter = 0;
+                //Console.WriteLine("Resetting jump counter");
             }
         }
 
@@ -834,6 +861,7 @@ namespace SliceEngine
         {
             if (IsGround(other))
             {
+                //Console.WriteLine($"Colliding with {other}");
                 groundContactCount++;
             }
         }
@@ -842,9 +870,14 @@ namespace SliceEngine
         {
             if (IsGround(other))
             {
+              //  Console.WriteLine($"Exit Colliding with {other}");
+
                 groundContactCount--;
                 if (groundContactCount < 0)
+                {
                     groundContactCount = 0;
+                    Console.WriteLine("Gonna reset jump counter");
+                }
             }
         }
         private bool IsGround(uint id)
