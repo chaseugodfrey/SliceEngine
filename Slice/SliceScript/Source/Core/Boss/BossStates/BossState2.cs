87c4791d1db6c885127c1cdec50e65dce2e95be7
Apperu <b.muhammadhafiz@digipen.edu>
2026-03-06 22:00:03 +0800

    reorg boss folder + test states + stop tracking imgui.ini

---
+++ Slice/SliceScript/Source/Core/Boss/BossStates/BossState2.cs
@@ -0,0 +1,41 @@
+using SliceEngine;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using System.Threading;
+
+
+namespace SliceEngine
+{
+    // Move up and down like a sine wave forever
+    public class BossState2 : State
+    {
+        float timer = 0f;
+        float amplitude = 2f; // up/down range
+        float frequency = 2f; // speed of oscillation
+        Vector3 startPosition;
+        Transform transform;
+
+        public BossState2(uint bossId, uint playerId) : base(bossId, playerId) { }
+
+        public override void Enter()
+        {
+            transform = GetComponent<Transform>();
+            startPosition = transform.Position;
+            timer = 0f;
+        }
+
+        public override void Update(float dt)
+        {
+            timer += dt;
+            float yOffset = (float)Math.Sin(timer * frequency) * amplitude;
+            transform.Position = startPosition + new Vector3(0, yOffset, 0);
+
+        }
+
+        public override void Exit()
+        {
+        }
+    }
+}
