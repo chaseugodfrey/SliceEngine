592e9f0646aedb55f4c738b3d960a505974f54fc
Apperu <b.muhammadhafiz@digipen.edu>
2026-03-07 15:58:28 +0800

    groundslamparticlefx enemylvl2

--- Slice/SliceScript/Source/Core/Enemy/EnemyLevel2.cs
+++ Slice/SliceScript/Source/Core/Enemy/EnemyLevel2.cs
@@ -380,6 +380,7 @@ namespace SliceEngine
                 {
                     if (!slam.onCooldown && slam.attacking)
                     {
+                        CreateGameObject("Prefabs/GroundSlamParticleFX.prefab").GetComponent<Transform>().Position = transform.Position - new Vector3(0, 2.0f, 0);
                         ToggleHitbox(true);
                         //slam.ToggleHitbox(true);
                         slam.onCooldown = true;
