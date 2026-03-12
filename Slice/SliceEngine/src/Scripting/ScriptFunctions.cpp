a03352e99768eec3733b0b487b31100265babd3a
Rainnyu <t.rainii375@gmail.com>
2026-03-13 03:58:45 +0800

    castShadow change script

--- Slice/SliceEngine/src/Scripting/ScriptFunctions.cpp
+++ Slice/SliceEngine/src/Scripting/ScriptFunctions.cpp
@@ -2851,6 +2851,27 @@ namespace SliceEngine
 #pragma endregion
 
 #pragma region Material
+	static void Renderer_SetCastShadow(uint32_t entityID, bool castShadow)
+	{
+		GameObject GO = FactoryInstance.GetGOByEntity((Entity)entityID);
+
+		if (GO.HasComponent<Renderer>())
+		{
+			auto& renderer = GO.GetComponent<Renderer>();
+			renderer.castShadow = castShadow;
+		}
+	}
+	static void Renderer_GetCastShadow(uint32_t entityID, bool* castShadow)
+	{
+		GameObject GO = FactoryInstance.GetGOByEntity((Entity)entityID);
+
+		if (GO.HasComponent<Renderer>())
+		{
+			auto& renderer = GO.GetComponent<Renderer>();
+			*castShadow = renderer.castShadow;
+		}
+	}
+
 	static void Material_SetColor(uint32_t entityID, glm::vec4* color)
 	{
 		GameObject GO = FactoryInstance.GetGOByEntity((Entity)entityID);
@@ -3269,6 +3290,9 @@ namespace SliceEngine
 		ADD_INTERNAL_CALL(SpriteRenderer_SetEnabled);
 		ADD_INTERNAL_CALL(SpriteRenderer_SetColor);
 		ADD_INTERNAL_CALL(SpriteRenderer_GetColor);
+
+		ADD_INTERNAL_CALL(Renderer_SetCastShadow);
+		ADD_INTERNAL_CALL(Renderer_GetCastShadow);
 
 		// Material
 		ADD_INTERNAL_CALL(Material_SetColor);
