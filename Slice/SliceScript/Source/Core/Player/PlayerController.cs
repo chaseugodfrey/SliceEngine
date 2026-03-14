d2dbc78cee368c0dd4b059d493600ba7f85035f6
Apperu <b.muhammadhafiz@digipen.edu>
2026-03-15 05:33:21 +0800

    oops all parents

--- Slice/SliceScript/Source/Core/Player/PlayerController.cs
+++ Slice/SliceScript/Source/Core/Player/PlayerController.cs
@@ -237,12 +237,6 @@ namespace SliceEngine
             //    SliceLog.Log(PlayerCurrentAttack.ToString());
             //    OnCombatStateChange();
             //}
-
-            if (FXDash != null)
-            {
-                Transform t = FXDash.GetComponent<Transform>();
-                t.Position = transform.Position;
-            }
         }
 
         public override void OnFixedUpdate(float dt)
@@ -1178,6 +1172,7 @@ namespace SliceEngine
                 FXDash = CreateGameObject("Prefabs/FX_PlayerDash.prefab");
                 FXDash.GetComponent<Transform>().Position = transform.Position;
                 FXDash.GetComponent<Transform>().Rotation = transform.Rotation;
+                FXDash.SetParent(gameObject);
 
                 Console.WriteLine($"Dashing now, after state is : {PlayerMovementState.ToString()}");
             }
