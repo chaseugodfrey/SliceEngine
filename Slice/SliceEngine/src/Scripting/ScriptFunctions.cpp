5496387b7db550fa095e097ca007fcc9d5bc52a6
chaseugodfrey <143150195+chaseugodfrey@users.noreply.github.com>
2026-03-15 20:19:20 +0800

    added unscaled delta time retrieval in c#

--- Slice/SliceEngine/src/Scripting/ScriptFunctions.cpp
+++ Slice/SliceEngine/src/Scripting/ScriptFunctions.cpp
@@ -30,6 +30,7 @@ DigiPen Institute of Technology is prohibited.
 #include "../Input/ActionMapping.h"
 #include "Graphics/RenderManager.h"
 #include "../Systems/LayerManager.h"
+#include "../Systems/FramerateManager.h"
 
 #pragma warning(push)
 #pragma warning(disable : 4002)
@@ -2931,6 +2932,11 @@ namespace SliceEngine
 		SliceEngine::Core::GetInstance()->GetSceneSystem()->SetTimeScale(timeScale);
 	}
 
+	static float Time_GetDeltaTimeUnscaled()
+	{
+		return SliceEngine::Core::GetInstance()->GetFramerateManager()->getDeltaTime();
+	}
+
 #pragma region Application
 
 	static MonoString* Application_GetFilePath()
@@ -2998,9 +3004,9 @@ namespace SliceEngine
 		ADD_INTERNAL_CALL(Scene_UnloadCurrentScene);
 
 		// Time
+		ADD_INTERNAL_CALL(Time_GetDeltaTimeUnscaled);
 		ADD_INTERNAL_CALL(Time_SetTimeScale);
 		ADD_INTERNAL_CALL(Time_GetTimeScale);
-	
 
 		//Camera
 		ADD_INTERNAL_CALL(Camera_SetMainCamera);
--- Slice/SliceScript/Source/Core/Systems/Time.cs
+++ Slice/SliceScript/Source/Core/Systems/Time.cs
@@ -16,6 +16,16 @@ namespace SliceEngine
         [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Unity-style lowercase naming for familiarity")]
         public static float time { get; private set; }
 
+        [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Unity-style lowercase naming for familiarity")]
+        public static float deltaTimeUnscaled 
+        {
+            get
+            {
+                return FunctionCalls.Time_GetDeltaTimeUnscaled();
+            }
+        }
+
+        [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Unity-style lowercase naming for familiarity")]
         public static float timeScale
         {
             get
@@ -37,7 +47,6 @@ namespace SliceEngine
         // public static float timeScale = 1.0f;
 
         //public static float scaledDeltaTime => deltaTime * timeScale;
-        public static float unscaledDeltaTime => deltaTime;
         public static float fps => deltaTime > 0 ? 1f / deltaTime : 0f;
 
         public void OnCreate()
