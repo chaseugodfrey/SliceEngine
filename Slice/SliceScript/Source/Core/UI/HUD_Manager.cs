16616a1e37efad7eb2ae4bdde6576514bf4e7a19
zekingfx <zekingfx@gmail.com>
2026-03-19 03:16:54 +0800

    Tried new rotating enemy material (kinda ug). Fixed spawn in circle. Fixed dialogue skipping first of the set.

--- Slice/SliceScript/Source/Core/UI/HUD_Manager.cs
+++ Slice/SliceScript/Source/Core/UI/HUD_Manager.cs
@@ -202,7 +202,7 @@ namespace SliceEngine
                 inputOpen = false;
                 dialogueDone = true;
                 //Bootstrap.Player.canInput = true;
-                dialogueIndex = 0;
+                dialogueIndex = -1;
                 //levelDialogues.Clear();
                 SetTextBox("");
                 CloseTextBox();
