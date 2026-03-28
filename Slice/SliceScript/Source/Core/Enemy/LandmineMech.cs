f1bc6c9fc79bf129ee4909f1752ee726236325e4
Apperu <b.muhammadhafiz@digipen.edu>
2026-03-28 23:33:34 +0800

    boom boom boom

--- Slice/SliceScript/Source/Core/Enemy/LandmineMech.cs
+++ Slice/SliceScript/Source/Core/Enemy/LandmineMech.cs
@@ -1,5 +1,6 @@
 using SliceEngine;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.Remoting.Metadata.W3cXsd2001;
 
@@ -7,5 +8,40 @@ namespace SliceEngine
 {
     public class LandmineMech : SliceBehaviour
     {
+        bool triggered = false;
+
+        GameObject explosion;
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+            GameObject[] children = gameObject.GetAllChildren();
+            foreach (GameObject child in children)
+            {
+                if (child.tag == "LandmineExplosionCore")
+                {
+                    explosion = child;
+                }
+            }
+        }
+
+        public override void OnTriggerEnter(uint other)
+        {
+            if (triggered) { return; }
+            triggered = true;
+            base.OnTriggerEnter(other);
+            GameObject collidedGO = FindGameObjectWithID(other);
+            if (collidedGO != null && collidedGO.tag == "Player")
+            {
+                explosion.As<LandmineMechExplosion>().Triggered();
+                StartCoroutine(Suicide());
+            }
+        }
+
+        IEnumerator Suicide()
+        {
+            yield return new WaitForSeconds(5.0f);
+            gameObject.Destroy();
+        }
     }
 }
