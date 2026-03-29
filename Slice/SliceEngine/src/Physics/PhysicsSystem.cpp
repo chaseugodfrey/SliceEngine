77d480c8cc46e428f7a5b4971d603205f116978b
AloysiusTeo <teo.k@digipen.edu>
2026-03-29 15:04:19 +0800

    cloning fixed

--- Slice/SliceEngine/src/Physics/PhysicsSystem.cpp
+++ Slice/SliceEngine/src/Physics/PhysicsSystem.cpp
@@ -1348,7 +1348,7 @@ namespace SliceEngine
 		auto& collider = mRegistry->get<ColliderShape>(entity);
 
 		CreateJoltBody(entity);
-		OnColliderModified(reg, entity);
+
 		UpdateShapeFromTransform(entity);
 		OnColliderModified(reg, entity);
 
