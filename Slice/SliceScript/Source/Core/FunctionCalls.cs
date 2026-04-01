2c6a4818b435c2cfa5b34df64728f6ead7468e1d
chaseugodfrey <143150195+chaseugodfrey@users.noreply.github.com>
2026-04-01 11:50:55 +0800

    readding gamma/stopallsound back + audiosource.enablepathfinding

--- Slice/SliceScript/Source/Core/FunctionCalls.cs
+++ Slice/SliceScript/Source/Core/FunctionCalls.cs
@@ -22,6 +22,11 @@ namespace SliceEngine
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
         internal extern static void Camera_SetMainCamera(uint entityID);
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        internal extern static void Camera_SetGamma(float gamma);
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        internal extern static float Camera_GetGamma();
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
         internal extern static void Camera_ToggleImpactFrames(uint entityID, bool isEnable);
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
         internal extern static void Camera_SetImpactFrameWorldPosition(uint entityID, ref Vector3 position);
@@ -226,6 +231,8 @@ namespace SliceEngine
         internal extern static void Audio_SetMute(uint entityID, bool mute);
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
         internal extern static bool Audio_GetMute(uint entityID);
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        internal extern static void Audio_StopAllSound();
 
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
         internal extern static void Audio_SetSoundName(uint entityID, ref Audio audioName);
