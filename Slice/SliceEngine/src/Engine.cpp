68353db01dfa41159689e62702fb0d83129fbd25
lyongyee <69913917+lyongyee@users.noreply.github.com>
2026-04-02 17:13:58 +0800

    switched spriteAnim to timeunscaled

--- Slice/SliceEngine/src/Engine.cpp
+++ Slice/SliceEngine/src/Engine.cpp
@@ -1097,7 +1097,7 @@ namespace SliceEngine
 
 
 		frm->StartSystem("Sprite Animation");
-		sSpriteAnim.Update(deltaTimeScaled);
+		sSpriteAnim.Update(deltaTimeUnscaled);
 		frm->EndSystem("Sprite Animation");
 
 		frm->StartSystem("Canvas");
