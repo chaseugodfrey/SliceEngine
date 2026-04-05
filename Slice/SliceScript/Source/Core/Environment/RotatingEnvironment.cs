4c74903a60eb29d087cba476d812868ccb951c24
chaseugodfrey <143150195+chaseugodfrey@users.noreply.github.com>
2026-04-05 21:58:58 +0800

    level 3 stuff - need to load credits

--- Slice/SliceScript/Source/Core/Environment/RotatingEnvironment.cs
+++ Slice/SliceScript/Source/Core/Environment/RotatingEnvironment.cs
@@ -29,54 +29,18 @@ namespace SliceEngine
 
         public override void OnFixedUpdate(float dt)
         {
-            if (!active) return; 
+            if (active)
+            {
+                base.OnFixedUpdate(dt);
 
-            base.OnFixedUpdate(dt);
-
-            //push it forward based on speed
-
-            if (currentSpiralRate <= 0f)
-                return;
-
-            this.transform.Rotate((360f / currentSpiralRate) * dt, spiralAxis);
-
-            //this.GetComponent<Transform>().Position += this.GetComponent<Transform>().Forward.Normalize() * speed * dt; 
+                this.transform.Rotate((360f / currentSpiralRate) * dt, spiralAxis);
+            }
         }
 
         // starts or stops rotating, depending on the value of toBeActive, over the course of timer seconds
         public void StartOrStopRotating(bool toStart, float seconds)
         {
-            StartCoroutine(Ramp(toStart, seconds));
-        }
-
-        IEnumerator Ramp(bool toBeActive, float timer)
-        {
-            if (toBeActive)
-            {
-                currentSpiralRate = 0.01f;
-                active = true;
-            }
-
-            float elapsedTime = timer;
-            while (elapsedTime >= 0.0f)
-            {
-                float rate = toBeActive ? 1f - elapsedTime / timer : elapsedTime / timer;
-                if (rate > 0.0f)
-                {
-                    rate = 1.0f / rate;
-                }
-
-                currentSpiralRate = rate * spiralRate;
-                elapsedTime -= Time.deltaTime;
-                yield return null;
-            }
-
-            if (!toBeActive)
-            {
-                currentSpiralRate = 0.0f;
-                active = false;
-            }
-
+            active = toStart;
         }
     }
 }
