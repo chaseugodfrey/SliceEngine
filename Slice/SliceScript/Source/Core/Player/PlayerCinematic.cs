ccd5ab5a77181089bf144feb7e6acb1fdc1b05de
Gideon Francis <gideonnfrancis999@gmail.com>
2026-04-06 04:42:09 +0800

    UI Bar fade back for cinematic

--- Slice/SliceScript/Source/Core/Player/PlayerCinematic.cs
+++ Slice/SliceScript/Source/Core/Player/PlayerCinematic.cs
@@ -53,7 +53,7 @@ namespace SliceEngine
             //  cinematicCamera.GetComponent<Transform>().Position = cameraStartingPos.GetComponent<Transform>().WorldPosition;
             Camera.SetMainCamera(cinematicCamera);
             GetComponent<Animator>().SetBool("Cinematic", true);
-            StartCoroutine(UIAnimation());
+            StartCoroutine(UIAnimation(true));
 
             AudioSettings.PlaySFX("03_02_HQ_OurLastShot");
             Bootstrap.HUDManager.PlayDialogueForLevel(69, 2, true, true);
@@ -177,7 +177,7 @@ namespace SliceEngine
             if (animState == 0)
             {
                 GetComponent<Animator>().SetBool("Cinematic", true);
-                StartCoroutine(UIAnimation());
+                StartCoroutine(UIAnimation(true));
             }
 
             if (animState == 1)
@@ -189,6 +189,7 @@ namespace SliceEngine
             {
                 //Bootstrap.CameraController.LockCamera = false;
                 //Bootstrap.Player.SetPlayerLock(false);
+                StartCoroutine(UIAnimation(false));
             }
 
 
@@ -265,13 +266,20 @@ namespace SliceEngine
             //CoroutineManager.StopAllCoroutines(_transitionRunner);
         }
 
-        public IEnumerator UIAnimation()
+        public IEnumerator UIAnimation(bool moveIn)
         {
             float elapsedTime = 0f;
             float startingTopBar = topBar.GetComponent<RectTransform>().Pos_Y;
             float startingBottomBar = bottomBar.GetComponent<RectTransform>().Pos_Y;
+
             float endingTopBar = startingTopBar - 75.0f;
             float endingBottomBar = startingBottomBar + 75.0f;
+
+            if (!moveIn)
+            {
+                endingTopBar = startingTopBar + 75.0f;
+                endingBottomBar = startingBottomBar - 75.0f;
+            }
 
             while (elapsedTime < uiDuration)
             {
