a631a28e9b4710dfba76579d27e59f1051bc867c
AloysiusTeo <teo.k@digipen.edu>
2026-04-05 19:24:42 +0800

    single threaded(havent updated event callbacks)

--- Slice/SliceEngine/src/Physics/PhysicsSystem.cpp
+++ Slice/SliceEngine/src/Physics/PhysicsSystem.cpp
@@ -45,7 +45,7 @@ namespace SliceEngine
 		SLICE_LOG("Physics System Shutdown");
 	}
 
-	bool PhysicsSystem::Initialize( size_t tempAllocatorSize, JPH::uint maxBodies, JPH::uint numBodyMutex, JPH::uint maxContactConstraints, JPH::uint threadCount)
+	bool PhysicsSystem::Initialize( size_t tempAllocatorSize, JPH::uint maxBodies, JPH::uint numBodyMutex, JPH::uint maxContactConstraints)
 	{
 		if (isInitialized)
 		{
@@ -53,14 +53,14 @@ namespace SliceEngine
 		}
 		try
 		{
-			if (threadCount == 0)
-			{
-				threadCount = std::thread::hardware_concurrency() - 1;
-				if (threadCount == 0)
-				{
-					threadCount = 2;  // Fallback if hardware_concurrency() returns 0
-				}
-			}
+			//if (threadCount == 0)
+			//{
+			//	threadCount = std::thread::hardware_concurrency() - 1;
+			//	if (threadCount == 0)
+			//	{
+			//		threadCount = 2;  // Fallback if hardware_concurrency() returns 0
+			//	}
+			//}
 
 			//Jolt uses function pointers for memory allocation, sets up the function pointers Jolt uses internally.
 			JPH::RegisterDefaultAllocator();
@@ -77,7 +77,7 @@ namespace SliceEngine
 
 			tempAllocator = std::make_unique<JPH::TempAllocatorImpl>(tempAllocatorSize);
 
-			jobSystem = std::make_unique<JPH::JobSystemThreadPool>(JPH::cMaxPhysicsJobs, JPH::cMaxPhysicsBarriers, threadCount);
+			jobSystem = std::make_unique<JPH::JobSystemSingleThreaded>(JPH::cMaxPhysicsJobs);
 
 			broadphaseLayerInterface = std::make_unique<BPLayerInterfaceImpl>();
 			objectVsBroadphaseLayerFilter = std::make_unique<ObjectVsBroadPhaseLayerFilterImpl>(broadphaseLayerInterface.get());
